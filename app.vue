73d966d2865c22917b0f61d1f5ee780ab18c2397
Peet Stander <standerpm@omni-biz.com>
2022-04-13 09:37:16 +0200

    update

---
+++ app.vue
@@ -0,0 +1,13 @@
+<script setup lang="ts">
+const icons = ['i-mdi-alarm', 'i-mdi-menu', 'i-mdi-account', 'i-mdi-email', 'i-mdi-book']
+</script>
+<template>
+  <div>
+    <h1>UNOCSS icons</h1>
+    <div v-for="(item, index) in icons" :key="index">
+      <div class="flex">
+    Name:  {{item}} Icon: <div :class="item"></div>
+      </div>
+    </div>
+  </div>
+</template>
